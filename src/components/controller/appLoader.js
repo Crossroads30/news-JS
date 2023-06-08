4a710ddd2a6aa93d93b40693c2b6c77e1a68db84
Crossroads30 <Crossroads30@mail.ru>
2023-06-08 09:35:39 +0300

    feat: add api to project

--- src/components/controller/appLoader.js
+++ src/components/controller/appLoader.js
@@ -1,11 +1,11 @@
 import Loader from './loader';
 
 class AppLoader extends Loader {
-    constructor() {
-        super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
-        });
-    }
+   constructor() {
+      super('https://newsapi.org/v2/', {
+         apiKey: '4d4c1c8ea5944e83badde09e1c9e38ac', // получите свой ключ https://newsapi.org/
+      });
+   }
 }
 
 export default AppLoader;
